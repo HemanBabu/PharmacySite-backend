e010b7336a045520d89de6b688e4b5640fdead90
HemanBabuSeervi <hemanbabuseervi@gmail.com>
2025-04-02 20:02:25 +0530

    cleanup

--- middlewares/optionalVerifyJWT.js
+++
@@ -1,9 +0,0 @@
-const verifyJWT = require("./verifyJWT.js");
-
-function optionalVerifyJWT(req, res, next){
-    const token = req.headers["authorization"]?.split(" ")[1];
-    if(token && token!="null") verifyJWT(req, res, next);
-    else next();
-}
-
-module.exports = optionalVerifyJWT;
--- models/userModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-    user : {type : String, required : true, unique : true},
-    password : {type : String, required : true}
-});
-
-module.exports = new mongoose.model("user", userSchema);
